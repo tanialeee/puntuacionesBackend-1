be661268481c1ce487d00975fac7eed679cc2245
kant003 <kant003@gmail.com>
2020-02-04 15:49:47 +0100

    Add coors

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ var app = express();
 app.use( bodyParser.urlencoded( {extended:false}) )
 app.use( bodyParser.json() )
 
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+
 app.get('/', (req, res)=>{
     res.status(200).send("Hola Angel");
 })
